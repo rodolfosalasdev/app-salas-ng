1155ac2e294bf9a96549585457577b5efc506981
Rodolfo Salas de Lima <rodolfosalas@Rodolfos-MacBook-Pro.local>
2024-08-21 15:03:38 -0300

    chore: schematics, standalone, inlinestyle and onpush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { HeaderComponent } from './view/header/header.component';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app-salas-ng';
