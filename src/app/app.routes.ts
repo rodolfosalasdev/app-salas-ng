2cb5eb675551596768a97d4bc131aa032d2cfc74
Rodolfo Salas de Lima <rodolfosalas@Rodolfos-MacBook-Pro.local>
2024-08-23 11:25:33 -0300

    chore: conf routerLink messenger

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MessengerComponent } from './components/messenger/messenger.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    { path: '', component: HomeComponent },
+    { path: 'messenger', component: MessengerComponent, pathMatch: 'full' }
 ];
